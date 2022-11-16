021983eaf9670d7634510fa0953e7b2e09eb7eda
Tim Havlicek <t.havlicek@s-v.de>
2022-11-16 10:49:03 +0100

    meter

--- client/client.ts
+++ client/client.ts
@@ -1,6 +1,7 @@
 console.log("client");
 
 import { Peer } from "peerjs";
+import AudioStreamMeterVertecal from "./components/AudioMeterVertical";
 
 function log(msg: string) {
   document.write(`
@@ -10,13 +11,13 @@ function log(msg: string) {
   `);
 }
 
-function init() {
+async function init() {
   const peer = new Peer("cool-luckydye-peer-id-client");
 
   peer.on("call", (call) => {
     log("calling");
 
-    call.on("stream", (remoteStream) => {
+    call.on("stream", async (remoteStream) => {
       log("recieved stream");
 
       console.log(remoteStream.getAudioTracks());
@@ -26,6 +27,12 @@ function init() {
       video.oncanplay = () => {
         video.play();
       };
+
+      const audioCtxt = new AudioContext();
+      await audioCtxt.audioWorklet.addModule("./audio/meter.js");
+      const meter = new AudioStreamMeterVertecal(audioCtxt, "output");
+      meter.setSourceStream(remoteStream);
+      document.body.append(meter);
     });
     call.answer(null);
   });
